eebc47c10a307e10ecde10fb06c33c74a180c521
arkadacasinos <bezdep.rf@mail.ru>
2026-05-28 23:13:49 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -92,7 +92,7 @@ export const metadata: Metadata = {
     yandex: process.env.NEXT_PUBLIC_YANDEX_SITE_VERIFICATION,
   },
   other: {
-    "yandex-verification": process.env.NEXT_PUBLIC_YANDEX_SITE_VERIFICATION ?? "ef020fac5a31d3eb",
+    "yandex-verification": process.env.NEXT_PUBLIC_YANDEX_SITE_VERIFICATION ?? "2194be96d9ffacbc",
   },
 }
 
